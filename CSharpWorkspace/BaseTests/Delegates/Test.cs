135ba9bd1d04a7c3755ee545903ee0673b23b245
Anthony Geiser <antho.geiser@gmail.com>
2016-10-10 21:48:56 +0200

    Initialize solution and base unit test project

---
+++ CSharpWorkspace/BaseTests/Delegates/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BaseTests.Delegates
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void DelegateTest()
+        {
+            var entity = new Entity<string>();
+            var initString = default(string);
+            Assert.AreEqual(entity.Data, initString);
+
+            var oldValue = initString;
+            var newValue = "New Value";
+            entity.ValueChanged += (s, e) =>
+            {
+                Assert.AreEqual(e.OldValue, oldValue);
+                Assert.AreEqual(e.NewValue, newValue);
+            };
+
+            entity.Data = newValue;
+        }
+    }
+}
---
+++ CSharpWorkspace/BaseTests/NUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BaseTests
+{
+    [TestFixture]
+    public class NUnitTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.That(true == true);
+        }
+    }
+}
