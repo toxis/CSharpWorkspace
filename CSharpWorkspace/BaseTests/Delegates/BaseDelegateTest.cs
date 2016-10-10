586cee87c47f0d586411af9da021b3fc722ca9f1
Anthony Geiser <antho.geiser@gmail.com>
2016-10-10 22:03:15 +0200

    Add base reflection test

--- CSharpWorkspace/BaseTests/Delegates/BaseDelegateTest.cs
+++ CSharpWorkspace/BaseTests/Delegates/BaseDelegateTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using NUnit.Framework;
+using BaseTests.Common;
 
 namespace BaseTests.Delegates
 {
---
+++ CSharpWorkspace/BaseTests/Reflections/BaseReflectionTest.cs
@@ -0,0 +1,39 @@
+﻿using BaseTests.Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseTests.Reflections
+{
+    [TestFixture]
+    public class Test
+    {
+        private Entity<string> _entity;
+        private const string HelloReflection = "Hello Reflection";
+
+        [SetUp]
+        public void Setup()
+        {
+            _entity = new Entity<string>();
+            _entity.Data = HelloReflection;
+        }
+
+        [Test]
+        public void PropertyTest()
+        {
+            var properties = _entity.GetType().GetProperties();
+            Assert.AreEqual(properties.Count(), 1);
+            var propertyInfo = properties.Single();
+            Assert.AreEqual(propertyInfo.Name, nameof(_entity.Data));
+            var value = propertyInfo.GetValue(_entity);
+            Assert.AreEqual(value, HelloReflection);
+
+            var newValue = "New Value";
+            propertyInfo.SetValue(_entity, newValue);
+            Assert.AreEqual(newValue, _entity.Data);
+        }
+    }
+}
