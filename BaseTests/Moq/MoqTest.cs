28f869384125e78e1a356f916c6a117a3ebcd168
Anthony Geiser <a.geiser@panipro.ch>
2018-07-18 14:17:53 +0200

    Improve Moq test with Linq and verify if method was correctly called

--- BaseTests/Moq/MoqTest.cs
+++ BaseTests/Moq/MoqTest.cs
@@ -9,9 +9,10 @@ namespace BaseTests.Moq
         [Test]
         public void MultiplyCalculatorTest()
         {
-            var mockCalculator = new Mock<MoqCalculator>();
-            mockCalculator.Setup(c => c.Multiply(2, 2)).Returns(4);
-            Assert.AreEqual(4, mockCalculator.Object.Pow2(2));
+            var calculator = Mock.Of<MoqCalculator>(c => c.Multiply(2, 2) == 4);
+            Assert.AreEqual(4, calculator.Pow2(2));
+            // Be sure that Multiply method was called in Pow2 method
+            Mock.Get(calculator).Verify(c => c.Multiply(2, 2));
         }
     }
 }
