661b68dc0ce8995145793d983ab62f3ad1f3374e
Anthony Geiser <a.geiser@panipro.ch>
2018-07-18 14:09:12 +0200

    Add Moq test

---
+++ BaseTests/Moq/MoqCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BaseTests.Moq
+{
+    public class MoqCalculator
+    {
+        public virtual int Multiply(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Pow2(int number)
+        {
+            return Multiply(number, number);
+        }
+    }
+}
---
+++ BaseTests/Moq/MoqTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Moq;
+
+namespace BaseTests.Moq
+{
+    [TestFixture]
+    public class MoqTest
+    {
+        [Test]
+        public void MultiplyCalculatorTest()
+        {
+            var mockCalculator = new Mock<MoqCalculator>();
+            mockCalculator.Setup(c => c.Multiply(2, 2)).Returns(4);
+            Assert.AreEqual(4, mockCalculator.Object.Pow2(2));
+        }
+    }
+}
